5e2ba0c77dbdb3dc9bfc996cf5ad52fa80ab2258
piotros <piotros@wp.pl>
2016-03-03 00:39:37 +0100

    Add index view for development

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>miblo</title>
+</head>
+<body>
+
+<div id="app"></div>
+
+<script src="http://localhost:8080/app.min.js"></script>
+
+</body>
+</html>
