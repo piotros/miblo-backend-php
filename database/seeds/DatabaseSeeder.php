07c1197f4af2546df870fdedc90ca4272d414267
piotros <piotros@wp.pl>
2016-05-08 14:45:31 +0200

    Add basic database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserTableSeeder::class);
+        factory('App\Entry', 10)->create();
+        // factory('App\User', 10)->create();
     }
 }
